a0c98e8adf53bf53d61d368ac90debfcd76fa16c
NewCodeLearner <Girish.barhate@gmail.com>
2024-01-17 10:52:01 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score ="33"
+
+console.log(typeof score);
+
+let valueinnumber = Number(score)
+
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+//"33" ==> 33
+//"33abc" ==> NaN
+
+let isLoggedIn =1
+let boolean = Boolean(isLoggedIn)
+console.log(boolean);
+
+//1 => true
+//0 => false
+//'' => false
+//"girish" ==> true
