01f52b9299797344abe39ecbb7340c59c5662244
NewCodeLearner <Girish.barhate@gmail.com>
2024-01-17 10:17:58 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all Js code as newer version
+
+//alert("hello") we are using node js not browser
+let name ="Girish";
+let age =37;
+let isLoggedIn = false;
+let value = null;
+let alert =undefined;
+
+//symbols => unique
+
+//object
+
+console.table([name,age,isLoggedIn,value,alert]);
+console.log(typeof(name)); //string 
+console.log(typeof(value)); //null is an object
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 122445
+let accountemail ="girish@gmail.com"
+var accountpassword="12345"
+city ="Pune"
+let accountstate;
+
+console.log(accountId);
+console.table([accountId,accountemail,accountpassword,city,accountstate])
