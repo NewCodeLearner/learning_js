e9fd0dad5fc1367967650fa100776fbfdfd00636
NewCodeLearner <Girish.barhate@gmail.com>
2024-01-17 11:30:46 +0000

    summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,3 +18,21 @@ console.log(boolean);
 //0 => false
 //'' => false
 //"girish" ==> true
+
+
+
+//***********Operations */
+
+let value =3
+let negativalue = -value
+console.log(negativalue);
+
+let str1 ="Hello"
+let str2 =" Girish"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2 +2)
+console.log(1 + 2 +"2")
---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,23 @@
+//primitive
+
+//7 types : String , Number, Boolean , Null, undefined,Symbol,BigInt
+
+//reference type or  non primitive
+
+//Arrays , Objects , Functions
+
+const heroes = ["shaktiman","nagraj","doga"];
+
+let obj ={
+    name:"Girish",
+    age : 37,
+    city: "Pune"
+
+}
+
+
+const myfunction = function(){
+    console.log("Hello World!")
+
+}
+
