5cd29a1178957e73b2620cf71b3f3a2676e2d3c8
NewCodeLearner <Girish.barhate@gmail.com>
2024-01-18 04:26:21 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,37 @@
+const myArr = [0,1,2,3,4,5] 
+//console.log(myArr[1])//zero base indexing
+const myheroes = ["superman","batman","hulk"]
+
+const newArr = new Array(1,2,3,4,5)
+//console.log(newArr[1])//zero base indexing
+//console.log(newArr[4])//zero base indexing
+
+
+//Array Methods
+
+//myArr.push(6)
+//myArr.push(7)
+//console.log(myArr)//zero base indexing
+//myArr.pop()
+//console.log(myArr)//zero base indexing
+
+//myArr.unshift(9)
+//myArr.shift(9)
+
+//console.log(myArr.includes(5))
+
+const mynewarr = myArr.join()
+console.log(myArr)
+//console.log(mynewarr) //joins and converts to string
+
+//slice
+console.log(myArr.slice(1,3))
+
+console.log(myArr.splice(1,3)) // splice removes the portion from hte original array
+console.log(myArr) 
+
+
+
+
+
+
