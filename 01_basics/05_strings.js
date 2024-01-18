24e45026893f7a5ffd7e10b96f88571e9498bf18
NewCodeLearner <Girish.barhate@gmail.com>
2024-01-18 03:52:31 +0000

    numbers and math

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,8 +5,15 @@ const repos = 50
 //string interpolation
 console.log (`Hello my name is ${name} and my repo count is ${repos}`)
 
-const gamename = new String("Girish")
+const gamename = new String("Girish-hc-dot")
 
 console.log(gamename[0])
 console.log(gamename.length)
 console.log(gamename.toUpperCase())
+
+const url ="https://www.girish%20barhate.com"
+
+console.log(url.replace("%20", "-"));
+console.log(url.includes("girish"))
+
+console.log(gamename.split("-"))
