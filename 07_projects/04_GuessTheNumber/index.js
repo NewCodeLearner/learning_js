49cdc657880dcfb3463d56438a0a3a806b982a92
NewCodeLearner <Girish.barhate@gmail.com>
2024-01-19 08:46:42 +0000

    stiucture modifications

---
+++ 07_projects/04_GuessTheNumber/index.js
@@ -0,0 +1,39 @@
+const ranmdomnum = Math.floor((Math.random()*100+1))
+console.log(ranmdomnum)
+
+const submit =document.querySelector('#subt');
+const userInput =document.querySelector('#guessField');
+const guessLot =document.querySelector('.guesses');
+const remaining =document.querySelector('.lastResult');
+const lowOrHi =document.querySelector('.lowOrHi');
+const startOver =document.querySelector('.resultParas');
+
+
+const p = document.createElement('p')
+
+let prevguess = [];
+
+let numGuess =1
+
+let playgame=true;
+
+function validateGuess(guess){
+
+
+}
+
+function checkGuess(guess){
+
+
+}
+
+function displayGuess(guess){
+
+
+}
+
+function displayMessage(message){
+
+
+}
+
