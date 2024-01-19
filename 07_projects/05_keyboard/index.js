60430116569ebf83b008e248bff7000fb44a6c19
NewCodeLearner <Girish.barhate@gmail.com>
2024-01-19 14:00:07 +0000

    project 5 solution

---
+++ 07_projects/05_keyboard/index.js
@@ -0,0 +1,19 @@
+const refinsert = document.getElementById('insert')
+
+window.addEventListener('keydown',(e)=>{
+    refinsert.innerHTML =`
+    <table>
+    <tr>
+      <th>Key</th>
+      <th>Keycode</th> 
+      <th>Code</th>
+    </tr>
+    <tr>
+      <td>${e.key === ' ' ? 'Space' : e.key}</td>
+      <td>${e.keyCode}</td> 
+      <td>${e.code}</td>
+    </tr>
+    
+  </table>
+    `
+})
